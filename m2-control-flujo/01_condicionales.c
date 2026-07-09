97401ebde54efe903a77d2c7901cbc69c75bc06f
thonydoo <anpaico@gmail.com>
2026-07-09 02:23:57 +0000

    es mi mejor racha

--- m2-control-flujo/01_condicionales.c
+++ m2-control-flujo/01_condicionales.c
@@ -16,23 +16,74 @@ int main(void) {
 
     /* --- if / else if / else --- */
     printf("=== if/else ===\n");
-    float temperatura = 38.5f;
+    float temperatura = 23.5f;
     printf("Temperatura: %.1f C\n", temperatura);
     /* TODO: if/else que clasifique: >40→"CRITICO", >35→"ALERTA", >25→"NORMAL", sino→"FRIO" */
+    if (temperatura > 40)
+    {
+        printf("ESTADO : CRITICO \n");
+    }
+    else if (temperatura > 35)
+    {
+        printf("ESTADO : ALERTA \n");
+    }
+    else if (temperatura > 25)
+    {
+        printf("ESTADO : NORMAL \n");
+    }
+    else
+    {
+        printf("ESTADO : FRIO \n");
+    }
+
+
 
     /* --- Condiciones compuestas --- */
     printf("\n=== && y || ===\n");
     float humedad = 75.0f;
     /* TODO: si temperatura>35 Y humedad>70 -> "Activar ventilacion" */
-
+    if((temperatura > 35) && (humedad > 70))
+    {
+        printf("ACTIVAR VENTILACION \n");
+    }
+    else
+    {
+        printf("VENTILACION APAGADA \n");
+    }
     bool bateria_baja = true, sensor_error = false;
     /* TODO: si bateria_baja O sensor_error -> imprimir cual es el problema */
-
+    if((bateria_baja == true) || (sensor_error == true))
+    {
+        if(bateria_baja)
+        {
+            printf("El problema es la bateria \n");
+        }
+        if(sensor_error)
+        {
+            printf("el problema es el sensor \n");
+        }
+    }
     /* --- switch --- */
     printf("\n=== switch ===\n");
     uint8_t estado = 2;
     printf("Estado: %u\n", estado);
     /* TODO: switch sobre estado: 0→"APAGADO", 1→"INIT", 2→"ACTIVO", 3→"ERROR", default→"DESCONOCIDO" */
+    switch(estado){
+        case 0:;
+            printf("APAGADO \n");
+            break;
+        case 1:;
+            printf("INIT \n");
+            break;
+        case 2:;
+            printf("ACTIVO \n");
+            break;
+        case 3:;
+            printf("ERROR \n");
+            break;
+        default:;
+            printf("DESCONOCIDO \n");
+    }
 
     char modo = 'A';
     /* TODO: switch sobre modo: 'M'→"MANUAL", 'A'→"AUTOMATICO", 'S'→"SLEEP" */
